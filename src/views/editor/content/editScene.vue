fff61ddd706841622ee831f73b121aa74d38bbc1
木易公子 <350926623@qq.com>
2022-07-12 21:05:43 +0800

    场景

---
+++ src/views/editor/content/editScene.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="edit-scene-wrap">
+    <span>Scene</span>
+    <el-input :model-value="modelValue" @input="inputChange"></el-input>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  modelValue: String
+})
+
+const emits = defineEmits(['update:modelValue']);
+
+const inputChange = (value: string) => {
+  emits('update:modelValue', value)
+}
+
+</script>
+
+<style lang="less" scoped>
+.edit-scene-wrap {
+  width: 100%;
+  border-radius: 10px;
+  box-shadow: 0 1px 4px #00152914;
+  background-color: #FFFFFF;
+  font-size: 20px;
+  font-weight: 500;
+  padding: 20px;
+  box-sizing: border-box;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  span {
+    margin-right: 20px;
+  }
+}
+</style>
--- src/views/editor/content/index.vue
+++ src/views/editor/content/index.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="editor-content-wrap">
     content
+    <EditScene v-model="scene"/>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted } from "vue";
+import EditScene from '@/views/editor/content/editScene.vue'
+import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
 const bookId = computed(() => route.query.bookId as string);
 const chapterId = computed(() => route.query.chapterId as string);
+
+const scene = ref('123')
 
 </script>
 
